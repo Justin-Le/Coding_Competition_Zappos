c4fa5018b396e8ef4b7d13edd75c5a4ef4af2269
Justin-Le <lejustin.lv@gmail.com>
2016-03-01 21:37:02 -0800

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# Coding_Competition_Zappos
-My solution to the March 2016 Coding Competition hosted by Zappos.
+# Zappos Coding Competition March 2016
+
+My solution accomplishes absolutely nothing that was asked for in the competition, but here it is. (See the rules <a href='https://challenge.zappos.biz/problems/number-chain/index.html'>here</a>.)
+
+I spent 3 hours.
+
+Here's my reasoning:
+* If the first number in the pair is larger than the second, only the third operation (adjacent bit-swapping) will reduce the first number and bring it closer to the second. If bit-swapping can't bring the first lower than the second, then no solution exists.
+* Once the first number has been reduced to a value lower than the second, find the difference between the two numbers (diff). Then, if diff appears as a digit in the first number, add it to the first number. This approach does not account for the possibility that subsequences longer than one digit can also be added to the first number.
+* If diff does not appear as a subsequence of the first number, then the second operation (split the number into two pieces and multiply them) would be used. I didn't get this far. 
+* Of course, all of the above steps only account for the case when the first number is larger than the second (and it doesn't do a complete job of that). If the first is smaller, then... I'm going to bed.
---
+++ solution.py
@@ -0,0 +1,94 @@
+import time
+
+f = open('zappos.txt', 'r')
+
+nums = [0]*2
+i = 0
+
+for line in f:
+    for token in line.split():
+        nums[i] = token
+        i = i + 1
+
+print('nums0: ', nums[0])
+print('nums1: ', nums[1])
+
+if (int(nums[0]) > int(nums[1])):
+    for i in range(len(nums)):
+        nums[i] = str(bin(int(nums[i]))[2:])
+    print(nums[0])
+    print(nums[1])
+    
+
+    while(int(nums[0], 2) > int(nums[1], 2)):
+        i = 0
+        oldnums = nums[0]
+        # time.sleep(1)
+
+        while( (i <= len(nums[0]) - 2) ):
+
+            print('nums0i: ', nums[0][i])
+            print('nums0i+1: ', nums[0][i+1])
+            if (nums[0][i] == '0'):
+                print('break', i)
+            elif (nums[0][i] == '1'):
+                print('Swap away, kids!')
+
+                numsDigits = [0]*(len(nums[0]))
+
+                for j in range(len(numsDigits)):
+                    numsDigits[j] = nums[0][j]
+
+                numsDigits[i] = numsDigits[i+1]
+                numsDigits[i+1] = '1'
+
+                print('numsDigits: ', numsDigits)
+
+                nums[0] = numsDigits[0]
+
+                for j in range(1, len(numsDigits)):
+                    nums[0] = nums[0] + numsDigits[j]
+
+                # nums[0][i] = nums[0][i+1] 
+                # nums[0][i+1] = '1' 
+            print('nums0: ', nums[0])
+            print(i)
+            i = i + 1
+
+            print('\n')
+
+        if oldnums == nums[0]:
+            print('Stop repeating')
+            break
+
+        nums[0] = int(nums[0], 2)
+        nums[1] = int(nums[1], 2)
+
+        diff = nums[1] - nums[0]
+
+        for i in range(len(str(nums[0]))):
+            if str(diff) == str(nums[0])[i]:
+                nums[0] = nums[0] + diff
+                break
+            else:
+                print(' ')
+
+        nums[0] = str(bin(nums[0])[2:])
+        nums[1] = str(bin(nums[1])[2:])
+ 
+    if (int(nums[0], 2) != int(nums[1], 2)):
+        print('No solution exists.')
+    else:
+        print('A solution exists, but I\'m going to bed now.')
+
+    # nums[0] = int(nums[0], 2)
+    # nums[1] = int(nums[1], 2)
+    print('The transformed pair: ', int(nums[0], 2), int(nums[1], 2))
+else:
+    print('Shaka Zulu')
+    
+f.close()
+# print(nums[0])
+# print(int(str(nums[0]), 2))
+# print(int(nums[0]))
+# print(int(nums[1]))
